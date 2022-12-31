5840d77519a80495b721ef161f77a265b32a14c8
Farshid Amirkhani <farshid.amirkhani@gmail.com>
2022-12-31 18:36:57 +0330

    Add project files.

---
+++ KavehNegarOtpSender/Controllers/MainController.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using ExcelMapper;
+
+namespace KavehNegarOtpSender.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MainController : ControllerBase
+{
+    private readonly string _nonBrakingSpace = ((char)0x00a0).ToString();
+    [HttpPost]
+    public async Task<IActionResult> SendOtp(IFormFile file, string apiKey, string template, int tokenCount = 0)
+    {
+        List<Receiver> receivers;
+        try
+        {
+            if (file.Length <= 0)
+            {
+                return BadRequest();
+            }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            await using var stream = file.OpenReadStream();
+            using var importer = new ExcelImporter(stream);
+            switch (tokenCount)
+            {
+
+                case 1:
+                    importer.Configuration.RegisterClassMap<ReceiverClassMap1>();
+                    break;
+                case 2:
+                    importer.Configuration.RegisterClassMap<ReceiverClassMap2>();
+                    break;
+                case 3:
+                    importer.Configuration.RegisterClassMap<ReceiverClassMap3>();
+                    break;
+                default:
+                    importer.Configuration.RegisterClassMap<ReceiverClassMap>();
+                    break;
+            }
+
+            var sheet = importer.ReadSheet();
+            sheet.HasHeading = false;
+            receivers = sheet.ReadRows<Receiver>().ToList();
+        }
+        catch (Exception ex)
+        {
+            return Problem("Open Excel Failed");
+        }
+
+        try
+        {
+
+
+            var api = new Kavenegar.KavenegarApi(apiKey);
+
+            foreach (var receiver in receivers)
+            {
+                receiver.Token = receiver.Token?.Replace(" ", _nonBrakingSpace);
+                receiver.Token1 = receiver.Token1?.Replace(" ", _nonBrakingSpace);
+                receiver.Token2 = receiver.Token2?.Replace(" ", _nonBrakingSpace);
+                var result = await api.VerifyLookup(receiver.PhoneNumber, receiver.Token, receiver.Token1, receiver.Token2, template);
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return Problem("Send Otp Failed");
+        }
+
+
+        return Ok();
+    }
+
+
+}
+
+
---
+++ KavehNegarOtpSender/Receiver.cs
@@ -0,0 +1,53 @@
+﻿using ExcelMapper;
+
+namespace KavehNegarOtpSender;
+
+public class Receiver
+{
+    public string PhoneNumber { get; set; }
+    public string? Token { get; set; }
+    public string? Token1 { get; set; }
+    public string? Token2 { get; set; }
+}
+
+public class ReceiverClassMap : ExcelClassMap<Receiver>
+{
+    public ReceiverClassMap()
+    {
+        Map(receiver => receiver.PhoneNumber).WithColumnIndex(0);
+
+
+    }
+}
+public class ReceiverClassMap1 : ExcelClassMap<Receiver>
+{
+    public ReceiverClassMap1()
+    {
+        Map(receiver => receiver.PhoneNumber).WithColumnIndex(0);
+        Map(receiver => receiver.Token).WithColumnIndex(1).WithInvalidFallback(null);
+
+
+    }
+}
+public class ReceiverClassMap2 : ExcelClassMap<Receiver>
+{
+    public ReceiverClassMap2()
+    {
+        Map(receiver => receiver.PhoneNumber).WithColumnIndex(0);
+        Map(receiver => receiver.Token).WithColumnIndex(1).WithInvalidFallback(null);
+        Map(receiver => receiver.Token1).WithColumnIndex(2).WithInvalidFallback(null);
+
+
+    }
+}
+public class ReceiverClassMap3 : ExcelClassMap<Receiver>
+{
+    public ReceiverClassMap3()
+    {
+        Map(receiver => receiver.PhoneNumber).WithColumnIndex(0);
+        Map(receiver => receiver.Token).WithColumnIndex(1).WithInvalidFallback(null);
+        Map(receiver => receiver.Token1).WithColumnIndex(2).WithInvalidFallback(null);
+        Map(receiver => receiver.Token2).WithColumnIndex(3).WithInvalidFallback(null);
+
+    }
+}
